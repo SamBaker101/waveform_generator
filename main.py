1c370f859ee39cc0e8e716b801cf879a1d1e0be7
Sam Baker <Sam.Baker101@gmail.com>
2025-06-25 08:59:37 -0400

    Added user prompt option for entering params(for use as executable)

--- main.py
+++ main.py
@@ -17,10 +17,31 @@ def play_wave(filename):
     play_obj = wave_obj.play()
     play_obj.wait_done()  # Wait until sound has finished playing
 
-def main():
+def get_params_args():
     freq_Hz = int(sys.argv[1]) if len(sys.argv) > 1 else 440
     duration_ms = int(sys.argv[2]) if len(sys.argv) > 2 else 1000
+    return freq_Hz, duration_ms
+
+def get_params_ui():
+    print("Enter frequency in Hz (default is 440):")
+    freq_Hz = input()
+    freq_Hz = int(freq_Hz) if freq_Hz.isdigit() else 440
+    print("Enter duration in milliseconds (default is 1000):")
+    duration_ms = input()
+    duration_ms = int(duration_ms) if duration_ms.isdigit() else 1000
+    return freq_Hz, duration_ms
+
+
+def main():
     sample_rate = 192000  
+    #freq_Hz, duration_ms = get_params_args()
+    freq_Hz, duration_ms = get_params_ui()
+    
+    if freq_Hz <= 0 or duration_ms <= 0:
+        print("Frequency and duration must be positive integers.")
+        return  
+    print("Generating a sine wave of " + str(freq_Hz) + "Hz for " + str(duration_ms) + "ms...")
+    
     filename = "out/sine_wave" + str(freq_Hz) + "_" + str(duration_ms) + ".wav"
 
     wave = generate_sine_wave(freq_Hz, duration_ms/1000.0, sample_rate)
